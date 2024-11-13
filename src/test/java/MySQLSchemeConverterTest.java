73e3d55c308657043406b581c00e4439ff75288b
Albert <albert.gomes@liferay.com>
2024-11-13 09:47:41 -0300

    Update MySQLSchemeConverterTest

--- src/test/java/MySQLSchemeConverterTest.java
+++ src/test/java/MySQLSchemeConverterTest.java
@@ -1,13 +1,13 @@
 import com.upgrade.tools.convert.MySQLSchemeConverter;
 import com.upgrade.tools.util.Print;
-import com.upgrade.tools.util.ResultsThreadLocal;
+
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.annotation.Testable;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author Albert Gomes Cabral
@@ -17,14 +17,12 @@ public class MySQLSchemeConverterTest extends MySQLSchemeConverter {
 
     @BeforeAll
     public static void loadTemplates() {
-       Print.info(
-              "Initializing tests to " +
-                       MySQLSchemeConverter.class.getName());
+       Print.info("Initializing tests to %s".formatted(
+               MySQLSchemeConverter.class.getName()));
     }
 
     @Test
     public void testLoadingFilesCase() throws Exception {
-
         MySQLSchemeConverter mySQLSchemeConverter =
                 new MySQLSchemeConverter();
 
@@ -32,25 +30,19 @@ public class MySQLSchemeConverterTest extends MySQLSchemeConverter {
                 _SOURCE_LIFERAY_SCHEME_SQL, _TARGET_LIFERAY_SCHEME_SQL,
                 _NEW_CUSTOMER_SCHEME_OUT_PUT_SQL);
 
-        if (ResultsThreadLocal.getResultsThreadLocal()) {
-            List<Map<String, String>> contentList =
-                    _getContentFromFile(
-                            _NEW_CUSTOMER_SCHEME_OUT_PUT_SQL,
-                            _EXPECTED_CUSTOMER_SCHEME_OUT_PUT_SQL);
+        List<Map<String, String>> contentList =
+                _getContentFromFile(
+                        _NEW_CUSTOMER_SCHEME_OUT_PUT_SQL,
+                        _EXPECTED_CUSTOMER_SCHEME_OUT_PUT_SQL);
 
-            Assertions.assertEquals(contentList.get(0), contentList.get(1));
-        }
-        else {
-             Print.error(
-                     "test testLoadingFilesCase fail", null);
-        }
+        assert contentList != null;
 
+        Assertions.assertEquals(contentList.get(0), contentList.get(1));
     }
 
     private List<Map<String, String>> _getContentFromFile(
             String newFileOutput, String expectedFileOutput) {
         return null;
-
     }
 
     private static final String _EXPECTED_CUSTOMER_SCHEME_OUT_PUT_SQL =
