4c510915de56ceb8772ced439c87a13afe22680e
Albert <albert.gomes@liferay.com>
2024-11-13 09:45:39 -0300

    Create SchemeConverterUtil to reuse common features

---
+++ src/main/java/com/upgrade/tools/util/SchemeConverterUtil.java
@@ -0,0 +1,28 @@
+package com.upgrade.tools.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+/**
+ * @author Albert Gomes Cabral
+ */
+public class SchemeConverterUtil {
+
+    public static String readContent(InputStream inputStream) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(inputStream))) {
+
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+
+            return stringBuilder.toString();
+        }
+    }
+
+}
