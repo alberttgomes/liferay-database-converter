3b3a89eced85fa04d04f60006e56b4e52b3e68bb
Albert <albert.gomes@liferay.com>
2024-11-27 09:18:12 -0300

    Use standard char set UTF_8 as default

--- src/main/java/com/upgrade/tools/util/SchemeConverterUtil.java
+++ src/main/java/com/upgrade/tools/util/SchemeConverterUtil.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Albert Gomes Cabral
@@ -12,7 +13,7 @@ public class SchemeConverterUtil {
 
     public static String readContent(InputStream inputStream) throws IOException {
         try (BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader(inputStream))) {
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
 
             StringBuilder stringBuilder = new StringBuilder();
             String line;
