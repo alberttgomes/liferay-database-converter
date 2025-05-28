56129dfd9ce82488df9f2939426f6bd0bea6e156
Rubens Soares <rubens.soares@liferay.com>
2025-05-28 16:53:08 -0300

    ECU-10027 Add an indexesName parameter of type List<String> to the converter abstract method in the SchemeConverter interface

--- src/main/java/com/upgrade/tools/converter/SchemeConverter.java
+++ src/main/java/com/upgrade/tools/converter/SchemeConverter.java
@@ -1,12 +1,14 @@
 package com.upgrade.tools.converter;
 
+import java.util.List;
+
 /**
  * @author Albert Gomes Cabral
  */
 public interface SchemeConverter {
 
     void converter(
-            String path, String sourceName, String targetName, String newName)
+            String path, String sourceName, String targetName, String newName, List<String> indexesName)
         throws Exception;
 
 }
